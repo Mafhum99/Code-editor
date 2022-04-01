4d973934321bdc8bb9e6b50b358d333dfceaa84d
Mafhum99 <Alizabert99@gmail.com>
2022-04-01 16:07:01 +0700

    new a code 2 alternative

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // let txt = document.querySelectorAll
 
 // variable element
-const txE=document.querySelectorAll('.tte')
+const txE=document.querySelector('.tte')
 const ifr=document.querySelector('iframe')
 const btn=document.querySelector('button')
 
@@ -9,13 +9,13 @@ const btn=document.querySelector('button')
 // click event when button is clicker
 btn.addEventListener("click",function(){
     const html = txE.textContent
-    iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
+    ifr.src = "data:text/html;charset=utf-8," + encodeURI(html);
 });
 
 // realtime event auto relog
 txE.addEventListener("keyup",function(){
     const html = txE.textContent
-    iframe.src= "data:text/html;charset=utf-8," + encodeURI(html);
+    ifr.src= "data:text/html;charset=utf-8," + encodeURI(html);
 });
 
 
